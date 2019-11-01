7400538d7b7307e9540a01e304a8edcef6aa2e64
Manish Goyal <manish@devitechnosolutions.com>
2019-11-01 13:22:07 +0530

    Fixed Bug

--- parent_tabdata.php
+++ parent_tabdata.php
@@ -6,7 +6,7 @@
 $parent_tab_info_array=array(1=>'My Home Page',2=>'Marketing',3=>'Sales',4=>'Support',5=>'Analytics',6=>'Inventory',7=>'Tools',8=>'Settings');
 
 
-$parent_child_tab_rel_array=array(1=>array(),2=>array(83,83,),3=>array(6,4,2,7,),4=>array(81,81,),5=>array(),6=>array(23,34,),7=>array(8,15,25,26,27,33,47,46,45,55,56,57,73,27,69,38,57,58,66,67,71,72,73,82,82,84,84,),8=>array(),);
+$parent_child_tab_rel_array=array(1=>array(28,),2=>array(83,83,),3=>array(6,4,2,7,),4=>array(81,81,87,87,88,88,),5=>array(),6=>array(34,),7=>array(8,15,24,25,26,27,33,47,46,45,55,56,57,73,24,27,69,38,57,58,66,67,71,72,73,82,82,84,84,),8=>array(),);
 
 
 
