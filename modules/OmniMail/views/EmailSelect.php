14d94c38313af1b62640cae88adb1b04be4d933a
Manish Goyal <manish@devitechnosolutions.com>
2019-11-06 12:02:31 +0530

    Deleted Unwanted Folders and Modules

--- modules/OmniMail/views/EmailSelect.php
+++
@@ -1,8 +0,0 @@
-<?php
-class OmniMail_EmailSelect_View extends Vtiger_BasicAjax_View{
-    public function process(Vtiger_Request $request) {
-        $viewer = $this->getViewer($request);
-        $viewer->view('EmailSelect.tpl', "OmniMail", false);
-    }
-}
-?>
