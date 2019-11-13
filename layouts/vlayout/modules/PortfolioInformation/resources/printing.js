6ef0c782d81a98f7ec0bb6b46ee19f2217ab4110
Manish Goyal <manish@devitechnosolutions.com>
2019-11-13 10:16:08 +0530

    Removed Extra Files

--- layouts/vlayout/modules/PortfolioInformation/resources/printing.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-    jQuery.PrintReport = function PrintReport(report_type){
-//        alert("PRINT ME");
-    }
-    
-    $(document).on("click", "[name=PRINT]", function(e){
-        $.PrintReport("overview");
-    });
-});
