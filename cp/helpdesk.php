13754b95e875e883262e06fbe169a77d300ef4e5
Manish Goyal <manish@devitechnosolutions.com>
2019-12-10 17:32:04 +0530

    Fixed portal

--- cp/helpdesk.php
+++ cp/helpdesk.php
@@ -26,10 +26,10 @@ include_once 'includes/top-header.php';
                     <div class="kt-subheader__toolbar">
             			<div class="kt-portlet__head-actions">
 							
-							<!--  <a href="edit-ticket.php" class="btn btn-brand btn-elevate btn-icon-sm">
+						    <button class="btn btn-brand btn-elevate btn-icon-sm createTicket">
 								<i class="la la-plus"></i>
 								New Ticket
-							</a> -->
+							</button>
 							
 						</div>
             		</div>
@@ -55,6 +55,72 @@ include_once 'includes/top-header.php';
 				</div>
 			</div>
 			
+			<div class="modal fade" id="createTicketModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" style="display: none;" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        
+                        <form class="form-horizontal recordEditView" id="createticket" method="post" action="" novalidate="novalidate">
+					
+        					<div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Create Ticket</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                </button>
+                            </div>
+                    
+                			<div class="modal-body">
+                				<div class="row">
+                    				<div class="col-md-8">
+                						<div class="form-group validate is-invalid">
+                							<label class="control-label">Title</label>
+                							<input type="text" class="form-control" name="ticket_title" id="ticket_title" placeholder="Enter Title" >
+                							<div class="help-block with-errors"></div>
+                						</div>
+                					</div>
+                				</div>
+                				<div class="row">
+                					<div class="col-md-8">
+                						<div class="form-group">
+                							<label class="control-label">Priority</label>
+                							<select class="form-control" id="ticketpriorities" name="ticketpriorities">
+                    							<option value="">Select an Option</option>
+                    							<option value="Low">Low</option>
+                    							<option value="Normal">Normal</option>
+                    							<option value="High">High</option>
+                    							<option value="Urgent">Urgent</option>
+                    						</select>
+                							<div class="help-block with-errors"></div>
+                						</div>
+                					</div>
+                				</div>
+                				<div class="row">
+                					<div class="col-md-8">
+                						<div class="form-group">
+                							<label class="control-label">Description</label>
+                							<textarea class="form-control" name="description" id="description" placeholder="Description" ></textarea>
+                							<div class="help-block with-errors"></div>
+                						</div>
+                					</div>
+                				</div>
+                				<div class="row">
+                					<div class="col-md-8">
+                						<div class="form-group">
+                							<label class="control-label">Due date</label>
+                							<input class="form-control"  type="date" id="cf_656" name="cf_656">
+                							<div class="help-block with-errors"></div>
+                						</div>
+                					</div>
+                				</div>
+                			</div>
+                    	
+                			<div class="modal-footer quickCreateActions">
+                    			<button class="btn" type="reset" data-dismiss="modal">Cancel</button>
+                    			<button class="btn btn-success" type="submit"><strong>Save</strong></button>
+                        	</div>
+                		</form>
+                    </div>
+                </div>
+            </div>
+            
 		</div>			
 		
 		
@@ -85,6 +151,48 @@ include_once 'includes/top-header.php';
     		dom: "<'row'<'col-sm-3'l><'col-sm-3'f><'col-sm-6'p>>" +
     			"<'row'<'col-sm-5'i><'col-sm-12'tr>>" +
     			"<'row'<'col-sm-5'i><'col-sm-7'p>>",
+    	});
+
+    	jQuery(document).on('click','.createTicket',function(){
+  			$('#createTicketModal').modal('show');
+  		});
+    	jQuery(document).ready(function() {
+
+    		var validator = $('#createticket').validate({
+        	    rules : {
+        	    	 ticket_title : "required",
+        	    	 description : "required",
+        	    	 ticketpriorities : "required"
+        	    },
+        	});
+		
+        	jQuery('#createticket').on('submit', function (e) {
+
+    	        e.preventDefault();
+    	        
+    	        if (validator.form()) {
+    	        	$('#createticket').waitMe({effect : 'orbit',text : 'Please wait...' });
+        	        $.ajax({
+        	            type: "POST",
+        	            url: 'save-ticket.php',
+        	            data: jQuery('#createticket').serialize(), 
+        	            success: function(result)
+        	            {
+            	            
+        	            	var data = JSON.parse(result);
+        	            	if(data.success){
+        	            		toastr.info('Password Changed Successfully');
+        	            		$('#createticket').waitMe('hide');
+        	            		$("#createTicketModal").modal('hide');
+        	            		location.reload();
+        	            	}
+        	            	
+        	            }
+        	        });
+    	        }
+    	        
+    	    });
+    	    
     	});
     </script>
 </html>    
