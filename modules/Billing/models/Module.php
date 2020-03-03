294e34d116157b3426387554bc98f13cc288b7be
Manish Goyal <manish@devitechnosolutions.com>
2020-03-03 17:13:39 +0530

    Fixed Rainbow Bugs

--- modules/Billing/models/Module.php
+++ modules/Billing/models/Module.php
@@ -2,4 +2,13 @@
 
 class Billing_Module_Model extends Vtiger_Module_Model
 {
+    
+    /**
+     * Function to check whether the module is an entity type module or not
+     * @return <Boolean> true/false
+     */
+    public function isQuickCreateSupported() {
+        return false;
+    }
+    
 }
