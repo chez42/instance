073a01b176419e9b2ee5720d80395cdee5caa72b
ryansandnes <rsandnes@glenmerrybowl.com>
2020-04-08 17:02:59 -0700

    - Updated so stellarnav should always navigate

--- libraries/stellarnav/js/OmniStellarNav.js
+++ libraries/stellarnav/js/OmniStellarNav.js
@@ -10,9 +10,13 @@ jQuery(document).ready(function($) {
     jQuery('.report_selection').click(function(e){
         var record = $('[name=record_id]').val();
         var report = $(this).data('report');
+        var newWindow = window.open("", "_blank");
+
         $.post("index.php", {module:'PortfolioInformation', action:'GetAccountNumbersFromRecord', record:record}, function(response){
             var accounts = $.parseJSON(response);
-            window.open("index.php?module=PortfolioInformation&view="+report+"&account_number="+accounts+"&calling_record="+record, "_blank");
+            var url = "index.php?module=PortfolioInformation&view="+report+"&account_number="+accounts+"&calling_record="+record;
+//            window.open("index.php?module=PortfolioInformation&view="+report+"&account_number="+accounts+"&calling_record="+record, "_blank");
+            return newWindow.location = url;
         });
     });
 });
