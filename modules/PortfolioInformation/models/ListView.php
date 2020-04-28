455ad02868c894e46606a8c4ff1fed4f64fdaba7
ryansandnes <rsandnes@glenmerrybowl.com>
2020-04-28 14:05:37 -0700

    - Added wkhtml to more 'drop down'

--- modules/PortfolioInformation/models/ListView.php
+++ modules/PortfolioInformation/models/ListView.php
@@ -1,7 +1,20 @@
 <?php
 
 class PortfolioInformation_ListView_Model extends Vtiger_ListView_Model{
-    
+    public function getListViewMassActions($linkParams) {
+        $massActionLinks = parent::getListViewMassActions($linkParams);
+
+        $massActionLink = array(
+            'linktype' => 'LISTVIEWMASSACTION',
+            'linklabel' => 'Get Report Pdf',
+            'linkurl' => 'javascript:PortfolioInformation_List_Js.triggerReportPdf("index.php?module='.$this->getModule()->getName().'&view=ReportPdf&mode=showSelectReportForm");',
+            'linkicon' => ''
+        );
+        $massActionLinks['LISTVIEWMASSACTION'][] = Vtiger_Link_Model::getInstanceFromValues($massActionLink);
+
+        return $massActionLinks;
+    }
+
     function getListViewEntries($pagingModel) {
         return parent::getListViewEntries($pagingModel);
         
