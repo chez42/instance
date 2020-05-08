190b99d3208c9304db558e67f5cd7efb420a62d2
ryansandnes <rsandnes@glenmerrybowl.com>
2020-05-08 13:20:48 -0700

    - Added ability for CRM to pull data from Custodian

---
+++ libraries/custodians/Schwab/cSchwabPrices.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once("libraries/custodians/cCustodian.php");
+
+/**
+ * Class cSchwabPortfolios
+ * This class allows the pulling of data from the custodian database
+ */
+class cSchwabPrices extends cCustodian
+{
+    private $prices_data;//Holds the pricing information
+
+    /**
+     * Returns an associative array of all requested prices as of the given date
+     * @param string $table
+     * @param null $date
+     * @return mixed
+     */
+    public function GetPricesDataForDate(string $date, array $symbols)
+    {
+        if(empty($symbols))
+            return 0;
+
+        global $adb;
+        $params = array();
+        $params[] = $date;
+
+        if (empty($this->columns))
+            $fields = "*";
+        else {
+            $fields = implode ( ", ", $this->columns );
+        }
+
+        $symbol_q = generateQuestionMarks($symbols);
+        $params[] = $symbols;
+
+        $query = "SELECT {$fields} FROM {$this->database}.{$this->table} 
+                  WHERE date = ? AND symbol IN ({$symbol_q})";
+        $result = $adb->pquery($query, $params, true);
+
+        if ($adb->num_rows($result) > 0) {
+            while ($r = $adb->fetchByAssoc($result)) {
+                $this->prices_data[$r['symbol']] = $r;
+            }
+        }
+
+        return $this->prices_data;
+    }
+
+    /**
+     * Returns an associative array of all requested prices between the given dates
+     * @param string $table
+     * @param null $date
+     * @return mixed
+     */
+    public function GetPricesDataBetweenDates(array $symbols, $start, $end)
+    {
+        if(empty($symbols))
+            return 0;
+
+        global $adb;
+        $params = array();
+        $params[] = $start;
+        $params[] = $end;
+
+        if (empty($this->columns))
+            $fields = "*";
+        else {
+            $fields = implode ( ", ", $this->columns );
+        }
+
+        $symbol_q = generateQuestionMarks($symbols);
+        $params[] = $symbols;
+
+        $query = "SELECT {$fields} FROM {$this->database}.{$this->table} 
+                  WHERE date BETWEEN ? AND ? AND symbol IN ({$symbol_q})";
+        $result = $adb->pquery($query, $params, true);
+
+        if ($adb->num_rows($result) > 0) {
+            while ($r = $adb->fetchByAssoc($result)) {
+                $this->prices_data[$r['symbol']][] = $r;
+            }
+        }
+        return $this->prices_data;
+    }
+
+    /**
+     * Returns the prices_data variable that was filled in from the last retrieve
+     * @return mixed
+     */
+    public function GetSavedPricingData(){
+        return $this->prices_data;
+    }
+}
---
+++ libraries/custodians/Schwab/cSchwabTransactions.php
@@ -0,0 +1,122 @@
+<?php
+
+require_once("libraries/custodians/cCustodian.php");
+
+/**
+ * Class cSchwabPortfolios
+ * This class allows the pulling of data from the custodian database
+ */
+class cSchwabTransactions extends cCustodian
+{
+    protected $transactions_data;//Holds the pricing information
+
+    protected function FillAccountNumbersFromRepCodes(){
+        global $adb;
+        $params = array();
+        $questions = generateQuestionMarks($this->rep_codes);
+        $params[] = $this->rep_codes;
+
+        $query = "SELECT t.trans 
+                  FROM {$this->database}.{$this->portfolio_table} p 
+                  JOIN {$this->database}.schwab_trimmed t ON t.original = p.account_number 
+                  WHERE rep_code IN ({$questions})
+                  ORDER BY file_date DESC";
+        $result = $adb->pquery($query, array($this->rep_codes), true);
+        if($adb->num_rows($result) > 0)
+            while($r = $adb->fetchByAssoc($result)){
+                $this->account_numbers[] = $r;
+            }
+    }
+    /**
+     * cSchwabPortfolios constructor.
+     * @param string $custodian_name
+     * @param string $database
+     * @param string $module
+     * @param string $transactions_table
+     * @param string $table (REFERS TO BALANCE TABLE)
+     */
+    public function __construct(string $custodian_name, string $database, string $module,
+                                string $portfolio_table, string $transactions_table, array $rep_codes){
+        $this->name = $custodian_name;
+        $this->database = $database;
+        $this->module = $module;
+        $this->portfolio_table = $portfolio_table;
+        $this->table = $transactions_table;
+        if(!empty($rep_codes)) {
+            $this->SetRepCodes($rep_codes);
+        }
+    }
+    /**
+     * Returns an associative array of all requested transactions as of the given date
+     * @param null $date
+     * @return mixed
+     */
+    public function GetTransactionsDataForDate($date=null){
+        global $adb;
+        $params = array();
+        $questions = generateQuestionMarks($this->account_numbers);
+        $params[] = $this->account_numbers;
+
+        if (empty($this->columns))
+            $fields = "*";
+        else {
+            $fields = implode ( ", ", $this->columns );
+        }
+
+        if(!$date)
+            $date = $this->GetLatestTransactionsDate("trade_date");
+        $params[] = $date;
+
+        $query = "SELECT {$fields} FROM {$this->database}.{$this->table} t
+                  WHERE account_number IN ({$questions}) AND trade_date = ?";
+        $result = $adb->pquery($query, $params, true);
+
+        if ($adb->num_rows($result) > 0) {
+            while ($r = $adb->fetchByAssoc($result)) {
+                $this->transactions_data[$r['account_number']] = $r;
+            }
+        }
+
+        return $this->transactions_data;
+    }
+
+    /**
+     * Returns an associative array of all requested transactions between the given dates
+     * @param null start
+     * @param null end
+     * @return mixed
+     */
+    public function GetTransactionsDataBetweenDates($start, $end){
+        global $adb;
+        $params = array();
+        $questions = generateQuestionMarks($this->account_numbers);
+        $params[] = $this->account_numbers;
+        $params[] = $start;
+        $params[] = $end;
+
+        if (empty($this->columns))
+            $fields = "*";
+        else {
+            $fields = implode ( ", ", $this->columns );
+        }
+
+        $query = "SELECT {$fields} FROM {$this->database}.{$this->table} 
+                  WHERE account_number IN ({$questions}) AND trade_date BETWEEN ? AND ?";
+        $result = $adb->pquery($query, $params, true);
+
+        if ($adb->num_rows($result) > 0) {
+            while ($r = $adb->fetchByAssoc($result)) {
+                $this->transactions_data[$r['account_number']][] = $r;
+            }
+        }
+        return $this->transactions_data;
+    }
+
+    /**
+     * Returns the transactions_data variable that was filled in from the last retrieve
+     * @return mixed
+     */
+    public function GetSavedTransactionsData(){
+        return $this->transactions_data;
+    }
+}
