c01e5345ae3a88171c9e288cfe94411dae4e7cac
Manish Goyal <manish@devitechnosolutions.com>
2020-06-17 11:21:47 +0530

    Deleted Extra Files

--- delete-module.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once "includes/main/WebUI.php";
-include_once 'vtlib/Vtiger/Module.php';
-
-$Vtiger_Utils_Log = true;
-
-$module = Vtiger_Module::getInstance('VTEHistoryLog');
-if ($module) $module->delete();
