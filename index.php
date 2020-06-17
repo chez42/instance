21bbca757c6be0f9ec0904dbee1f59e44a5d75d4
Manish <manish.goyal@omnisrv.com>
2020-06-17 14:35:35 +0000

    testing changes in gitlab repo

--- index.php
+++ index.php
@@ -16,5 +16,6 @@ include_once 'include/Webservices/Relation.php';
 include_once 'vtlib/Vtiger/Module.php';
 include_once 'includes/main/WebUI.php';
 
+
 $webUI = new Vtiger_WebUI();
 $webUI->process(new Vtiger_Request($_REQUEST, $_REQUEST));
