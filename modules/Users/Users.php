f8537ccba6c7f5614bfca367da901df037bc65e5
Manish Goyal <manish@devitechnosolutions.com>
2020-06-19 10:41:44 +0530

    Merged Changes from Live Repo

--- modules/Users/Users.php
+++ modules/Users/Users.php
@@ -397,7 +397,7 @@ class Users extends CRMEntity {
 				$query = "SELECT 1 from $this->table_name where user_name=? AND user_password=? AND status = ?";
 				$result = $this->db->requirePsSingleResult($query, array($usr_name, $encrypted_password, 'Active'), false);
 				if (empty($result)) {
-					return true;
+					return false;
 				} else {
 					return true;
 				}
