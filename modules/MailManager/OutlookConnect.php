bc3ecaf8a2ea4cf68a13fcd365832ff9de1d2872
Manish Goyal <manish@devitechnosolutions.com>
2020-07-17 11:42:11 +0530

    Fixed Oauth Redirect of Office through Common URL

--- modules/MailManager/OutlookConnect.php
+++ modules/MailManager/OutlookConnect.php
@@ -7,124 +7,19 @@ if(!isset($_SESSION['authenticated_user_id'])){
 }
 chdir(__DIR__.'/../../');
 
-require_once 'libraries/Office365/autoload.php';
-use Microsoft\Graph\Graph;
-use Microsoft\Graph\Model;
-
 require_once('includes/main/WebUI.php');
-
-$tenantId = '5fb735c9-4413-4ee6-902b-9bdf92480ca2';
 
 $clientId = MailManager_Office365Config_Connector::$clientId;
 
-$clientSecret = MailManager_Office365Config_Connector::$clientSecret;
-
 $redriectUri = MailManager_Office365Config_Connector::getRedirectUrl();
 
-$params = array(
-    'response_type=' . urlencode('code'),
-    'redirect_uri=' . urlencode($redriectUri),
-    'client_id=' . urlencode($clientId),
-    'scope=' . urlencode('User.Read Mail.ReadWrite Mail.Send offline_access'),
-);
+global $site_URL;
 
-$queryString = implode('&', $params);
+$auth_url = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?response_type=code&redirect_uri=".urlencode($redriectUri)."&client_id=".urlencode($clientId);
+$auth_url .= '&state=' . base64_encode(implode('||', array($site_URL, $_SESSION['authenticated_user_id'], "Office365", "MailManager")));
+$auth_url .= '&scope=' . urlencode('User.Read Mail.ReadWrite Mail.Send offline_access');
 
-if(isset($_REQUEST['error'])){
+header('Location: ' . $auth_url);
+
+exit;
     
-    echo "Access Denied";
-    
-    //WINDOW.CLOSE
-    
-} else if(!isset($_REQUEST['code'])){
-    
-    $auth_url = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?".$queryString;
-    header('Location: ' . $auth_url);
-    exit;
-    
-}else {
-    
-    $token_request_data = array(
-        "grant_type" => "authorization_code",
-        "code" => $_REQUEST['code'],
-        "redirect_uri" => $redriectUri,
-        "client_id" => $clientId,
-        "client_secret" => $clientSecret
-    );
-    
-    $token_request_body = http_build_query($token_request_data);
-    
-    $curl = curl_init('https://login.microsoftonline.com/common/oauth2/v2.0/token');
-    
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    
-    curl_setopt($curl, CURLOPT_POST, true);
-    
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $token_request_body);
-    
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-    
-    $response = curl_exec($curl);
-    
-    $response = json_decode($response, true);
-    
-  
-    if($response['access_token']){
-        
-        $accessToken = $response['access_token']; 
-        $refreshToken = $response['refresh_token'];
-        
-        try{
-            
-            $graph = new Graph();
-            $graph->setAccessToken($accessToken);
-            
-            $user = $graph->createRequest("GET", "/me")
-            ->setReturnType(Model\User::class)
-            ->execute();
-            
-            $displayName = $user->getDisplayName();
-            $userPrincipal = $user->getUserPrincipalName();
-            
-            $db = PearDatabase::getInstance();
-            $current_user_id = $_SESSION['authenticated_user_id'];
-            
-            $account_id = 1;
-            $maxresult = $db->pquery("SELECT max(account_id) as max_account_id FROM vtiger_mail_accounts", array());
-            if ($db->num_rows($maxresult)) $account_id += intval($db->query_result($maxresult, 0, 'max_account_id'));
-            
-            $db->pquery("UPDATE vtiger_mail_accounts SET set_default=? WHERE user_id=?",array(1, $current_user_id));
-            
-            $mailAccount = $db->pquery("SELECT * FROM vtiger_mail_accounts WHERE account_name = ? AND user_id=?",array($userPrincipal, $current_user_id));
-            
-            if($db->num_rows($mailAccount)){
-                
-                $db->pquery("UPDATE vtiger_mail_accounts SET display_name=?, account_name=?, mail_username=?,
-                mail_servername=?,set_default=?,from_name=?,from_email=?,access_token=?, refresh_token=?, user_id=?,
-                status=? WHERE account_name=? AND user_id=?",
-                    array($displayName,$userPrincipal,$userPrincipal,'Office365',0,$displayName,$userPrincipal,
-                        $accessToken,$refreshToken,$current_user_id,1,$userPrincipal,$current_user_id));
-                
-            }else{
-                $db->pquery("INSERT INTO vtiger_mail_accounts(account_id,display_name, account_name, mail_username,
-                mail_servername, set_default, from_name, from_email, access_token, refresh_token, user_id, status) 
-                VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
-                    array($account_id,$displayName,$userPrincipal,$userPrincipal,'Office365',0,$displayName,$userPrincipal,
-                        $accessToken,$refreshToken,$current_user_id,1));
-            }
-            
-            
-            echo "<script>window.opener.RefreshPage();window.close();</script>";
-            
-            exit;
-        
-        } catch(Exception $e){
-            
-            echo $e->getMessage();
-            
-        }
-       
-        
-    }
-    
-}
