a495eb2a07c789b83f8c5fb908c5f8d897f5f9bf
Manish Goyal <manish@devitechnosolutions.com>
2020-08-08 17:21:53 +0530

    Fixed few Bugs

---
+++ db-changes/Office365.php
@@ -0,0 +1,18 @@
+<?php
+chdir('../');
+include_once 'vtlib/Vtiger/Module.php';
+
+$Vtiger_Utils_Log = true;
+
+$MODULENAME = 'Office365';
+
+$db = PearDatabase::getInstance();
+
+$sel = $db->pquery("SELECT * FROM vtiger_tab WHERE name = 'Office365'");
+if (!$db->num_rows($sel)) {
+    $moduleInstance = new Vtiger_Module();
+    $moduleInstance->name = $MODULENAME;
+    $moduleInstance->parent= 'Tools';
+    $moduleInstance->isentitytype = false;
+    $moduleInstance->save();
+}
