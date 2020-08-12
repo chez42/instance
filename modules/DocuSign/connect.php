872613c82d8cf6a5814755d594f657b89bcf46d3
Manish Goyal <manish@devitechnosolutions.com>
2020-08-12 16:26:20 +0530

    Fixed Docusign

--- modules/DocuSign/connect.php
+++ modules/DocuSign/connect.php
@@ -22,39 +22,8 @@ if(DocuSign_Config_Connector::$server == 'Sandbox'){
 }
 if(DocuSign_Config_Connector::$server == 'Production')
     $api_client = new \DocuSign\eSign\Client\ApiClient($config);
-   
-if(isset($_GET['code'])){
-    
-    $token = $api_client->generateAccessToken(DocuSign_Config_Connector::$client_id, DocuSign_Config_Connector::$client_secret, $_GET['code']);
-    
-    $access_token = $token[0]['access_token'];
-    $refresh_token = $token[0]['refresh_token'];
-    $token_type = $token[0]['token_type'];
-    $expires_in = $token[0]['expires_in'];
-	
-	$adb = PearDatabase::getInstance();
-    
-    $current_user_id = $_SESSION['authenticated_user_id'];
-    
-	$result = $adb->pquery('SELECT * FROM vtiger_document_designer_configuration WHERE userid = ?',array($current_user_id));
-    
-	if($adb->num_rows($result)){
-        $adb->pquery("update vtiger_document_designer_configuration set access_token = ?,
-		refresh_token = ?, token_type = ?, expires_in = ? where userid = ?",
-            array($access_token, $refresh_token, $token_type, $expires_in, $current_user_id));
-    } else{
-        $adb->pquery("insert into vtiger_document_designer_configuration(userid,access_token,
-		refresh_token, token_type, expires_in) values(?,?,?,?,?)",
-            array($current_user_id, $access_token, $refresh_token, $token_type, $expires_in));
-    }
-    
-    echo "<script>window.opener.RefreshPage();window.close();</script>";
-    
-    exit;
-	
-} else {
-    
-    $auth_url = $api_client->getAuthorizationURI(DocuSign_Config_Connector::$client_id, 'signature', DocuSign_Config_Connector::getCallBackUrl(), 'code');
-    header("Location:$auth_url");
-    exit;
-}
+
+$state = base64_encode(implode('||', array($site_URL, $_SESSION['authenticated_user_id'], "Docusign", "Signature")));
+$auth_url = $api_client->getAuthorizationURI(DocuSign_Config_Connector::$client_id, 'signature', DocuSign_Config_Connector::getCallBackUrl(), 'code', $state);
+header("Location:$auth_url");
+exit;
