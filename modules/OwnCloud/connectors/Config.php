cf730281c8817d1131bef3e501fc19badaa47f4c
Manish Goyal <manish@devitechnosolutions.com>
2020-09-17 12:44:19 +0530

    Added OWN Cloud

---
+++ modules/OwnCloud/connectors/Config.php
@@ -0,0 +1,36 @@
+<?php
+/* +**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is: vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * ***********************************************************************************/
+
+Class OwnCloud_Config_Connector {
+    
+    static $username = null;
+    
+    static $password = null;
+    
+    static $url = null;
+    
+    public static function init(){
+        global $adb, $current_user;
+        
+        $result = $adb->pquery("SELECT * FROM vtiger_owncloud_credentials WHERE userid=?" , array($current_user->id));
+        
+        if($adb->num_rows($result)){
+            self::$username = $adb->query_result($result, 0, 'username');
+            self::$password = $adb->query_result($result, 0, 'password');
+            self::$url = $adb->query_result($result, 0, 'url');
+        }
+    }
+}
+
+OwnCloud_Config_Connector::init();
+
+
+
+
