860253200a3c1efeb33ae7c3babdfa8a2d481e34
ryansandnes <rsandnes@glenmerrybowl.com>
2020-10-02 10:56:52 -0700

    - Added realtime monitoring system for tracking issues without worrying about session interruption/freezing - Get location data from rep code returns location data from the table only for passed in rep codes - Positions now uses isset instead of empty to avoid crash - Added finished notification to Widget calculation service - Latest data service created to automatically search and parse 3 days worth of files.  This gets directories parsed without waiting on other cron jobs to do the work which allows for the pull to happen much sooner for instances

--- libraries/custodians/cFileHandling.php
+++ libraries/custodians/cFileHandling.php
@@ -73,6 +73,39 @@ class cFileHandling{
         return $tmp;
     }
 
+    public function GetLocationDataFromRepCode(array $rep_code){
+        global $adb;
+        $questions = generateQuestionMarks($rep_code);
+
+        $query = "SELECT id, custodian, tenant, file_location, rep_code, master_rep_code, omni_code, prefix, suffix, last_filename, 
+                         last_filedate, currently_active
+                  FROM custodian_omniscient.file_locations
+                  WHERE rep_code IN ({$questions})";
+        $result = $adb->pquery($query, array($rep_code));
+
+        $data = array();
+        if($adb->num_rows($result) > 0){
+            while($r = $adb->fetchByAssoc($result)){
+                $tmp = new cFileLocations();
+                $tmp->id = $r['id'];
+                $tmp->custodian = $r['custodian'];
+                $tmp->tenant = $r['tenant'];
+                $tmp->file_location = $r['file_location'];
+                $tmp->rep_code = $r['rep_code'];
+                $tmp->master_rep_code = $r['master_rep_code'];
+                $tmp->omni_code = $r['omni_code'];
+                $tmp->prefix = $r['prefix'];
+                $tmp->suffix = $r['suffix'];
+                $tmp->last_filename = $r['last_filename'];
+                $tmp->last_filedate = $r['last_filedate'];
+                $tmp->currently_active = $r['currently_active'];
+                $data[] = $tmp;
+            }
+        }
+
+        return $data;
+    }
+
     /**
      * Create new file location row
      * @param $data
