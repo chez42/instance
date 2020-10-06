987fd0d6d2c7b7f38bd20b83483182347385f2e2
Manish Goyal <manish@devitechnosolutions.com>
2020-10-06 12:02:29 +0530

    Fixed css for Menu

--- modules/OmniThemeManager/utils/customStyle.php
+++ modules/OmniThemeManager/utils/customStyle.php
@@ -890,6 +890,6 @@ tbody .ps__thumb-y{
 	color: <?= $text_contrast_menu ?>!important;
 }
 
-.stellarnav li li.has-sub > a:after {border-left: 6px solid <?= $text_contrast ?> !important;}
-.stellarnav li a {color: <?= $text_contrast ?> !important;}
+.stellarnav li li.has-sub > a:after {border-left: 6px solid <?= $text_contrast_menu ?> !important;}
+.stellarnav li a {color: <?= $text_contrast_menu ?> !important;}
 .stellarnav li {background-color:<?= $menu_color ?>;; border:<?= $menu_color ?>;}
