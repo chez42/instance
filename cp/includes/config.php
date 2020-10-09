46831a814c9266ce69e3994348da08090289cabf
Manish Goyal <manish@devitechnosolutions.com>
2020-10-09 17:14:58 +0530

    Fixed Bug

--- cp/includes/config.php
+++ cp/includes/config.php
@@ -23,7 +23,8 @@ $GLOBALS['portal_title'] = 'OMNI Client Portal';
 
 if(isset($_SESSION['ID']) && $_SESSION['ID'] != ''){
     
-    $GLOBALS['portal_logo'] = $_SESSION['portal_logo'];
+    if($_SESSION['portal_logo'] != '')
+        $GLOBALS['portal_logo'] = $_SESSION['portal_logo'];
     
 	$GLOBALS['portal_profile_image'] = $_SESSION['portal_profile_image'];
     
