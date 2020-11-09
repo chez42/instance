506501f939436c8f4e3008697c3dc3e67436479a
ryansandnes <rsandnes@glenmerrybowl.com>
2020-11-09 12:17:56 -0800

    - Added quick access ability to update an account number intelligently

---
+++ libraries/custodians/CustodianToOmni.php
@@ -0,0 +1,51 @@
+<?php
+
+DEFINE("TD", "TD");
+DEFINE("FIDELITY", "Fidelity");
+DEFINE("SCHWAB", "Schwab");
+DEFINE("PERSHING", "Pershing");
+
+class CustodianToOmni{
+    private $account_number;
+    private $custodian;//used for accessing the custodian database
+
+    public function __construct($account_number){
+        $this->account_number = $account_number;
+
+        switch(strtoupper(PortfolioInformation_Module_Model::GetCustodianFromAccountNumber($account_number))){
+            case "TD":
+                $this->custodian = TD;
+                break;
+            case "FIDELITY":
+                $this->custodian = FIDELITY;
+                break;
+            case "SCHWAB":
+                $this->custodian = SCHWAB;
+                break;
+            case "PERSHING":
+                $this->custodian = PERSHING;
+                break;
+        }
+    }
+
+    public function UpdatePositions(){
+        switch(strtoupper($this->custodian)){
+            CASE "TD":
+                $tmp = new cTDQuickAccess();
+                $tmp->PullPositions(array($this->account_number));
+                break;
+            CASE "FIDELITY":
+                $tmp = new cFidelityQuickAccess();
+                $tmp->PullPositions(array($this->account_number));
+                break;
+            CASE "SCHWAB":
+                $tmp = new cSchwabQuickAccess();
+                $tmp->PullPositions(array($this->account_number));
+                break;
+            case "PERSHING":
+#                $tmp = new cPershingQuickAccess();
+#                $tmp->PullPositions(array($this->account_number));
+                break;
+        }
+    }
+}
