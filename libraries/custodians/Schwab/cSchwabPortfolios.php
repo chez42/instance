668fea7f8d4e10ae68527b7381971ca2bbcf1082
ryansandnes <rsandnes@glenmerrybowl.com>
2021-02-08 11:22:45 -0800

    - BalanceBetweenDates for all custodians - Schwab updated to prevent transactions update error (still needs written)

--- libraries/custodians/Schwab/cSchwabPortfolios.php
+++ libraries/custodians/Schwab/cSchwabPortfolios.php
@@ -532,4 +532,28 @@ class cSchwabPortfolios extends cCustodian {
         }
         return $data;
     }
+
+    static public function BalanceBetweenDates(array $account_number, $sdate, $edate){
+        global $adb;
+        $questions = generateQuestionMarks($account_number);
+        $params = array();
+        $params[] = $account_number;
+        $params[] = $sdate;
+        $params[] = $edate;
+
+        $query = "SELECT account_number, account_value AS value, as_of_date AS date
+                  FROM custodian_omniscient.custodian_balances_fidelity 
+                  WHERE account_number IN ({$questions}) 
+                  AND as_of_date BETWEEN ? AND ?
+                  ORDER BY as_of_date";
+        $result = $adb->pquery($query, $params);
+
+        $data = array();
+        if($adb->num_rows($result) > 0){
+            while($r = $adb->fetchByAssoc($result)){
+                $data[$r['account_number']][] = $r;
+            }
+        }
+        return $data;
+    }
 }
