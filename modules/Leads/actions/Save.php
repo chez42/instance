9654a9beae721195c987b8df3ca4e43088390efe
Manish Goyal <manish@devitechnosolutions.com>
2021-03-10 11:09:30 +0530

    Modifications for salutation field

--- modules/Leads/actions/Save.php
+++ modules/Leads/actions/Save.php
@@ -9,15 +9,4 @@
  * All Rights Reserved.
  * *********************************************************************************** */
 
-class Leads_Save_Action extends Vtiger_Save_Action {
-
-	public function process(Vtiger_Request $request) {
-
-		//To stop saveing the value of salutation as '--None--'
-		$salutationType = $request->get('salutationtype');
-		if ($salutationType === '--None--') {
-			$request->set('salutationtype', '');
-		}
-		parent::process($request);
-	}
-}
+class Leads_Save_Action extends Vtiger_Save_Action {}
