fc7a43ff7317bdc57ba77523e384807517b7766b
ryansandnes <ryan.sandnes@omnisrv.com>
2021-05-13 18:24:45 -0700

    - Schwab transaction mapping speed increase

---
+++ modules/Settings/PortfolioInformation/views/SchwabTransactionMapping.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryansandnes
+ * Date: 2017-05-26
+ * Time: 3:30 PM
+ */
+
+class PortfolioInformation_SchwabTransactionMapping_View extends Vtiger_Index_View{
+    function process(Vtiger_Request $request) {
+        $mapping = PortfolioInformation_Administration_Model::GetSchwabTransactionMapping();
+        $activities = PortfolioInformation_Module_Model::GetActivityPicklistValues();
+
+        $viewer = $this->getViewer($request);
+        $viewer->assign("MAPPING", $mapping);
+        $viewer->assign("ACTIVITIES", $activities);
+        $viewer->assign("SCRIPTS", $this->getHeaderScripts($request));
+
+        $viewer->view('SchwabTransactionMapping.tpl', "PortfolioInformation", false);
+    }
+
+    public function getHeaderScripts(Vtiger_Request $request) {
+        $moduleName = $request->getModule();
+        $jsFileNames = array(
+            "modules.PortfolioInformation.resources.SchwabTransactionMapping", // . = delimiter
+        );
+        $jsScriptInstances = $this->checkAndConvertJsScripts($jsFileNames);
+        return $jsScriptInstances;
+    }
+}
+?>
