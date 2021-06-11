0c59d11eccbe4085769aaecfb90b032d61ba235a
Manish Goyal <manish@devitechnosolutions.com>
2021-06-11 13:20:17 +0530

    Added Billing Filter and Portfolio Recalculate Feature

--- layouts/v7/modules/PortfolioInformation/resources/List.js
+++ layouts/v7/modules/PortfolioInformation/resources/List.js
@@ -7,6 +7,61 @@
  * All Rights Reserved.
  ************************************************************************************/
 
-Vtiger_List_Js("PortfolioInformation_List_Js", {}, {
+Vtiger_List_Js("PortfolioInformation_List_Js", {
+	
+	ReCalculate : function() {
+    	var self = app.controller();
+        self.ReCalculate();
+    },
+
+	
+}, {
+	
+	ReCalculate : function() {
+        
+		var listInstance = this;
+		
+		var params = {};
+		
+		var url = 'index.php?module=PortfolioInformation&action=ReCalculate';
+		
+		var paramArray = url.slice(url.indexOf('?') + 1).split('&');
+		
+		for (var i = 0; i < paramArray.length; i++) {
+			var param = paramArray[i].split('=');
+			params[param[0]] = param[1];
+		}
+		
+		var listSelectParams = listInstance.getListSelectAllParams(true);
+		
+		listSelectParams = jQuery.extend(listSelectParams, params);
+		
+		if (listSelectParams) {
+			
+			listSelectParams['module'] = app.getModuleName();
+			
+			listSelectParams['action'] = 'ReCalculate';
+			
+			listSelectParams['search_params'] = JSON.stringify(listInstance.getListSearchParams());
+			
+			app.helper.showProgress();
+			
+			app.request.post({data: listSelectParams}).then(function (error, result) {
+				app.helper.hideProgress();
+				if(!error){
+					listInstance.loadListViewRecords().then(function (e) {
+						listInstance.clearList();
+						app.helper.showSuccessNotification({message: 'Portfolios ReCalculate Successfully'});
+					});
+				} else {
+					app.helper.showSuccessNotification({message: 'Error!!'});
+				}
+            });
+			
+			
+		} else{
+			listInstance.noRecordSelectedAlert();
+		}
+    },
 	 
 });
